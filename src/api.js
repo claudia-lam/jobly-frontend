bd817eee02460174ad6c36e2481f29a6f9da646c
Claudia Lam <claudiaslam@gmail.com>
2023-04-25 17:24:59 -0700

    add static methods for api call

--- src/api.js
+++ src/api.js
@@ -14,7 +14,8 @@ class JoblyApi {
   // Remember, the backend needs to be authorized with a token
   // We're providing a token you can use to interact with the backend API
   // DON'T MODIFY THIS TOKEN
-  static token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
+  static token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
     "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
     "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
 
@@ -22,10 +23,9 @@ class JoblyApi {
     console.debug("API Call:", endpoint, data, method);
 
     const url = `${BASE_URL}/${endpoint}`;
+    console.log("URL", url);
     const headers = { Authorization: `Bearer ${JoblyApi.token}` };
-    const params = (method === "get")
-        ? data
-        : {};
+    const params = method === "get" ? data : {};
 
     try {
       return (await axios({ url, method, data, params, headers })).data;
@@ -45,5 +45,24 @@ class JoblyApi {
     return res.company;
   }
 
-  // obviously, you'll add a lot here ...
+  /** Get list of all companies*/
+
+  static async getCompanies() {
+    let res = await this.request(`companies/`);
+    return res;
+  }
+
+  // /** Get details on a job by id. */
+
+  // static async getJob(id) {
+  //   let res = await this.request(`jobs/${id}`);
+  //   return res.job;
+  // }
+
+  /** Get list of all jobs*/
+
+  static async getJobs() {
+    let res = await this.request(`jobs/`);
+    return res;
+  }
 }
